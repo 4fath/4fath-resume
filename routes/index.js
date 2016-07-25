ddc02008c51a0e97f79ad06286cddcc368c9b461
fatih <fatihsenturk@mail.com>
2016-07-25 21:55:36 +0300

    added english version

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var path    = require("path");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('resume', { title: 'Fatih Şentürk - Resume' });
+router.get('/tr', function(req, res, next) {
+  res.sendFile(path.join(__dirname+'/resume.html'));
 });
+
+router.get('/en', function(req, res, next){
+	res.sendFile(path.join(__dirname+'/resume_en.html'));
+});
+
+router.get('/*', function(req, res, next){
+	res.redirect('/tr');
+});
+
+router.get('/tr*', function(req, res, next){
+	res.redirect('/tr');
+});
+
+
+router.get('/en*', function(req, res, next){
+	res.redirect('/en');
+});
+
 
 module.exports = router;
